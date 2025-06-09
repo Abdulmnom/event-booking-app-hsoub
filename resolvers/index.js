949f634052cdf31fc770bc68c8cc64e2156b7489
Abdulmoun <bdalhyalbryky@gmail.com>
2025-06-09 04:37:00 -0700

    DB Done

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,12 +1,6 @@
 const resolvers = {
     Query: {
-        hello: () => 'Hello, world! in the event books app from Hsoub',
-        goodbye: () => 'Goodbye, world!',
-        // books : () => [
-        //     { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
-        //     { title: 'The Catcher in the Rye', author: 'J.D. Salinger' },
-        //     { title: 'The Grapes of Wrath', author: 'John Steinbeck' }
-        // ]
+      
     },
     // Mutation: {
     //     greet: (_, { name }) => `Hello, ${name}!`
--- schema/index.js
+++ schema/index.js
@@ -2,8 +2,64 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type Query {
-        hello: String
-        goodbye: String!
+       events: [Event]
+       bookings: [Booking]
+       getUserEvents(userId: ID!): [Event]
+        getUserBookings(userId: ID!): [Booking]
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+    }
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        date: String!
+        price: Float!
+        creature: User!
+    }
+
+    type Booking {
+        _id: ID!
+        event: Event!
+        user: User!
+        createdAt: String!
+        updatedAt: String!
+    }
+
+    type Mutation {
+        creatUser(username: UserInput!) : User
+        createEvent(eventInput: EventInput!): Event
+        bookEvent(eventId: ID!): Booking
+        cancelBooking(booking: ID !) : Event
+        login(email: String!, password: String!) : AuthData
+        deleteEvent(eventId: ID!): [Event]
+    }
+
+    type AuthData {
+        userId: ID!
+        token: String!
+        username: String!
+        tokenExpiration: Int!
+    }
+    input UserInput {
+        username: String!
+        email: String!
+        password: String!
+
+    }
+    input EventInput {
+        title: String!
+        description: String!
+        date: String!
+        price: Float!
+    }
+    input BookingInput {
+        eventId: ID!
     }
 `;
 
